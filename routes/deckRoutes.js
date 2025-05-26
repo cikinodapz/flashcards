c778cc5393a49bca9a5232338d74b1a192893345
daffaagustians <dafagustiansaadi12@gmail.com>
2025-05-26 22:26:57 +0700

    dah sampe nambah navigasi

--- routes/deckRoutes.js
+++ routes/deckRoutes.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
-const { createDeck,getDecks,updateDeck,deleteDeck } = require('../controllers/deckController/deck');
+const { createDeck,getDecks,updateDeck,deleteDeck, getProfile } = require('../controllers/deckController/deck');
 const authMiddleware = require('../middlewares/auth.middleware'); // Middleware untuk verifikasi token
 
 const router = express.Router();
 
+router.get('/profile', authMiddleware, getProfile);
 router.post('/createDeck', authMiddleware, createDeck); // Hanya pengguna yang login yang bisa membuat deck
 router.get('/getAllDeck', authMiddleware, getDecks); // Hanya pengguna yang login yang bisa membuat deck
 router.put('/editDeck/:id', authMiddleware, updateDeck); // Hanya pengguna yang login yang bisa membuat deck
